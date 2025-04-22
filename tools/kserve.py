8828149883526f037361c9f74274fd60c755d982
geo.kim <geo.kim@juice.co.kr>
2025-04-22 14:47:11 +0900

    기본 flow 통과

--- tools/kserve.py
+++ tools/kserve.py
@@ -3,9 +3,21 @@ from typing import Any
 
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
+import requests
+import json
 
 class KserveTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        yield self.create_json_message({
-            "result": "Hello, world!"
-        })
+        try:
+            endpoint = tool_parameters.get('endpoint')
+            params = tool_parameters.get('params')
+            response = requests.post(endpoint, json={'instances':[json.loads(params)]})
+            data = response.json()
+            if 'error' in data.keys():
+                raise Exception(f'error occured: {data['error']}')
+            else:
+                yield self.create_json_message({
+                    "result": data['result']
+                })
+        except Exception as e:
+            raise Exception(f'error occured: {str(e)}')
