d1c8a82a19731041ee1b7a06a7c80c78febaf8f9
geo.kim <geo.kim@juice.co.kr>
2025-04-22 14:24:46 +0900

    init

--- tools/kserve.py
+++ tools/kserve.py
@@ -1,22 +1,11 @@
 from collections.abc import Generator
 from typing import Any
 
-import requests
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 class KserveTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        
-        endpoint = tool_parameters.get("endpoint")
-        params = tool_parameters.get('params')
-
-        try:
-            res = requests.post(endpoint, json=params)
-            body = res.json()
-            if 'error' in body.keys():
-                raise Exception(f'erro occured: {body['error']}')
-            else:
-                yield body['result']
-        except Exception as e:
-            raise Exception(f"An error occurred: {str(e)}")
+        yield self.create_json_message({
+            "result": "Hello, world!"
+        })
